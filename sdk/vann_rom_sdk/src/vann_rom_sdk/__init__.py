58e92b1325f68a74539afc3f6c3b36bab605a898
Lord-Xido <maxido3m@gmail.com>
2026-07-12 19:02:24 +0200

    Add VANN-ROM package exports

---
+++ sdk/vann_rom_sdk/src/vann_rom_sdk/__init__.py
@@ -0,0 +1,30 @@
+"""VANN-ROM Ω³ SDK.
+
+A reference implementation of a sparse 3D ROM bytecode virtual ANN processor.
+"""
+
+from .ann import TinyAutoencoder
+from .compiler import Assembler
+from .geometry import Address3D, morton3d_decode, morton3d_encode
+from .isa import Instruction, Opcode, NumericFormat, Phase
+from .rom import Sparse3DROM, VoxelPage
+from .vm import VANNVirtualMachine, VMConfig, VMResult
+
+__all__ = [
+    "Address3D",
+    "Assembler",
+    "Instruction",
+    "NumericFormat",
+    "Opcode",
+    "Phase",
+    "Sparse3DROM",
+    "TinyAutoencoder",
+    "VANNVirtualMachine",
+    "VMConfig",
+    "VMResult",
+    "VoxelPage",
+    "morton3d_encode",
+    "morton3d_decode",
+]
+
+__version__ = "0.1.0"
