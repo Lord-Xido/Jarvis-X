ce2813c5ada884048067f5a4c948f0c02c20f883
Lord-Xido <maxido3m@gmail.com>
2026-07-14 15:52:11 +0200

    Make reflex stabilization opt-in

--- src/jarvisx/reflex.py
+++ src/jarvisx/reflex.py
@@ -1,5 +1,10 @@
 class ReflexEngine:
+    def __init__(self, enabled=False):
+        self.enabled = bool(enabled)
+
     def stabilize(self, regs):
+        if not self.enabled:
+            return
         psi = regs["Ψ"]
         phi = regs["Φ"]
         delta = int(0.1 * (psi - phi))
