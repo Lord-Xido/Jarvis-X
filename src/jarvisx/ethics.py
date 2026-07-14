40812aa82fee2b66605847d08393f78ebc836db9
Lord-Xido <maxido3m@gmail.com>
2026-07-14 15:52:17 +0200

    Fail closed on unregistered opcodes

--- src/jarvisx/ethics.py
+++ src/jarvisx/ethics.py
@@ -1,9 +1,15 @@
+"""Opcode policy gate. Semantic policy belongs above the bytecode layer."""
+
+from .opcodes import OPCODE_NAMES
+
+
 class LambdaShield:
-    def __init__(self):
+    def __init__(self, allowed=None):
+        self.allowed = set(OPCODE_NAMES if allowed is None else allowed)
         self.blocked = set()
 
     def block(self, opcode):
-        self.blocked.add(opcode)
+        self.blocked.add(int(opcode))
 
     def allow(self, instr):
-        return instr.opcode not in self.blocked
+        return instr.opcode in self.allowed and instr.opcode not in self.blocked
