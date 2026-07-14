cf0812b4a13b69968e43cf232c1b9cb4280ceb77
Lord-Xido <maxido3m@gmail.com>
2026-07-14 17:31:54 +0200

    Remove SaaS import-time database side effect

--- src/jarvisx/saas/__init__.py
+++ src/jarvisx/saas/__init__.py
@@ -1,6 +1,5 @@
 """Dr Moagi Software Consultancy Cloud Network SaaS."""
 
-from .app import app
 from .service import ConsultancyService
 
-__all__ = ["app", "ConsultancyService"]
+__all__ = ["ConsultancyService"]
