baf8fc53c749d12d604358d1cf9511375bedf605
Lord-Xido <maxido3m@gmail.com>
2026-07-14 14:29:54 +0200

    Gate reflex mutation behind FLAGS bit

--- src/jarvisx/reflex.py
+++ src/jarvisx/reflex.py
@@ -1,5 +1,10 @@
+REFLEX_ENABLE_FLAG = 0x01
+
+
 class ReflexEngine:
     def stabilize(self, regs):
+        if not (regs["FLAGS"] & REFLEX_ENABLE_FLAG):
+            return
         psi = regs["Ψ"]
         phi = regs["Φ"]
         delta = int(0.1 * (psi - phi))
