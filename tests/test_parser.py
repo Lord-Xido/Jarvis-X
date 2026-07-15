b1b43398f49e241247921cf692480ed3fbd74d96
Lord-Xido <maxido3m@gmail.com>
2026-07-15 17:43:32 +0200

    Test parser comments and labels

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,6 @@
 from jarvisx.parser import Parser
 
-def test_parser():
-    ast = Parser().parse("SET Ψ 5\nHALT")
-    assert ast[0][0] == "SET"
+
+def test_parser_ignores_comments_and_preserves_labels():
+    ast = Parser().parse("SET Ψ 5 # input\nloop:\nHALT ; done")
+    assert ast == [["SET", "Ψ", "5"], ["loop:"], ["HALT"]]
