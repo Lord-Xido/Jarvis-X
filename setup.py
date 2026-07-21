abc7b9abea041a706485200cbaef1575e22645b1
Lord-Xido <maxido3m@gmail.com>
2026-07-21 17:03:04 +0200

    Align package metadata with tetration field

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ long_description = readme_file.read_text(encoding="utf-8") if readme_file.exists
 
 setup(
     name="jarvisx",
-    version="0.2.0",
-    description="Jarvis-X deterministic VM and sparse 3-D auto-encoding/decoding automaton",
+    version="0.3.0",
+    description="Jarvis-X deterministic VM and sparse tetration brick-field automaton",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Lord-Xido",
