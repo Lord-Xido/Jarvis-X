74619377abf7f9336f368f4c26c77c3f9b6cb149
Matladi Maxwell Moagi <maxido3m@gmail.com>
2026-08-21 08:25:54 +0200

    Export operational HyperCloud interfaces

--- src/jarvisx/cloud/__init__.py
+++ src/jarvisx/cloud/__init__.py
@@ -5,18 +5,28 @@ materialized state. Nothing in this package allocates a dense model matching
 the declared virtual parameter extent.
 """
 
+from .codec import LatentPacket, LosslessMultimodalCodec
 from .extent import HierarchicalAddress, SymbolicParameterExtent
 from .multimodal import MediaEnvelope, MediaKind
+from .operational import OperationalHyperCloud
+from .persistence import JobRecord, SQLiteStateStore
 from .routing import ShardCoordinate, SpatialShardRouter
 from .runtime import HyperCloudRuntime, SparseParameterStore
+from .worker import HyperCloudWorker
 
 __all__ = [
     "HierarchicalAddress",
     "HyperCloudRuntime",
+    "HyperCloudWorker",
+    "JobRecord",
+    "LatentPacket",
+    "LosslessMultimodalCodec",
     "MediaEnvelope",
     "MediaKind",
+    "OperationalHyperCloud",
     "ShardCoordinate",
     "SparseParameterStore",
     "SpatialShardRouter",
+    "SQLiteStateStore",
     "SymbolicParameterExtent",
 ]
