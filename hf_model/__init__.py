6187ca6d3a858a06c6a9f14e7e9e31b8f6034677
Matladi Maxwell Moagi <maxido3m@gmail.com>
2026-08-25 20:28:05 +0200

    Export inward self-optimizer API

--- hf_model/__init__.py
+++ hf_model/__init__.py
@@ -1,6 +1,20 @@
 """Hugging Face model implementation for Jarvis-X."""
 
 from .configuration_jarvisx import JarvisXConfig
+from .inward_self_optimizer import (
+    InwardOptimizerBounds,
+    InwardSelfOptimizer,
+    build_token_state_optimizer,
+    inward_fold,
+)
 from .modeling_jarvisx import JarvisXModel, JarvisXOutput
 
-__all__ = ["JarvisXConfig", "JarvisXModel", "JarvisXOutput"]
+__all__ = [
+    "JarvisXConfig",
+    "JarvisXModel",
+    "JarvisXOutput",
+    "InwardOptimizerBounds",
+    "InwardSelfOptimizer",
+    "build_token_state_optimizer",
+    "inward_fold",
+]
